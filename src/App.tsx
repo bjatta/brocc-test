d478ed88d245ff20e8356960fda79c8503c61bf2
bjatta <me@bjatta.ru>
2021-12-13 20:22:04 +0100

    styled component theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import './App.css';
 import Body from "./Example";
 import Counter from "./Counter";
 import {QueryClient, QueryClientProvider} from "react-query";
+import {ThemeProvider} from "styled-components";
+import theme from "./appearance/Theme";
+import StyledApp from "./appearance/StyledApp";
+
 const queryClient = new QueryClient()
 
 function App() {
     const [index, setIndex] = useState(0);
   return (
       <QueryClientProvider client={queryClient}>
-      <div className="App">
-      <Counter counter={index} setCounter = {setIndex}/>
-      <Body index={index}/>
-    </div>
+          <ThemeProvider theme={theme}>
+              <StyledApp className="App">
+                <Counter counter={index} setCounter = {setIndex}/>
+                <Body index={index}/>
+              </StyledApp>
+          </ThemeProvider>
       </QueryClientProvider>
   );
 }
---
+++ src/appearance/Theme.ts
@@ -0,0 +1,15 @@
+const dayTheme = {
+    backgroundColor: 'white',
+    color: 'black'
+};
+
+const nightThemde = {
+    backgroundColor: 'black',
+    color: 'lightgray'
+};
+
+const hour = (new Date()).getHours();
+
+const isDay = hour > 8 && hour < 18;
+
+export default isDay ? dayTheme : nightThemde;
